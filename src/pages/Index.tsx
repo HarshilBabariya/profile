1bce0c70023657267fc0792cca901c203a21925f
Harshil.21 <85223027+HarshilBabariya@users.noreply.github.com>
2025-06-30 10:38:49 +0000

    update link

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -347,7 +347,7 @@ const Index = () => {
                   <div className="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
                     <div className="flex space-x-3">
                       <Button size="sm" variant="ghost" className="text-white hover:text-purple-400">
-                        <a href={project.live}>
+                        <a href={project.live} className="flex items-center gap-1">
                         <ExternalLink className="w-4 h-4 mr-2" />
                         Live Demo
                         </a>
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/harshilbabariya.github.io/',
   server: {
     host: "::",
     port: 8080,
